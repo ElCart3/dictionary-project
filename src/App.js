3db003e330a4f31adf264c8c21b9136c64d36aff
ElCart3 <elliejane.carter@gmail.com>
2022-04-02 19:31:51 +0100

    Created form function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div className="container">
-          <div className="App">Hello from app</div>
+          <Dictionary />
           <footer>
             This page was coded by Ellie and is{" "}
             <a href="https://github.com/ElCart3/dictionary-project">
