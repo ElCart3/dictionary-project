f95e2323817d09edfa64ada0cdff92f1cbe66316
ElCart3 <elliejane.carter@gmail.com>
2022-04-02 19:16:11 +0100

    Created dictionary app with react, added bootstrap

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="App">Hello from app</div>
+          <footer>
+            This page was coded by Ellie and is <a href="#">open-sourced</a>
+          </footer>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
