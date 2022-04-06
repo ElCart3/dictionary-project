77c78d82089845d5960388f034676edc33779b4b
ElCart3 <elliejane.carter@gmail.com>
2022-04-06 19:56:26 +0100

    Display synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,16 +9,15 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
+            <div className="definition">{definition.definition}</div>
+            <div className="example">
               <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            </div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
+      <Synonyms synonyms={props.meaning.synonyms} />
     </div>
   );
 }
